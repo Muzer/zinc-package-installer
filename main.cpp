a9f1c954ddc43c7e97b3603a541ad515df67292f
TomMan81 <TomMan81@29953a18-a857-11de-8ba5-91705c1537ed>
2009-09-24 14:43:04 +0000

    Added arguments to ./zinc-package-installer so user can go ./zinc-package-installer [package].

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    if (argc > 1)
+    {
+        w.set_filename(string(argv[1]));
+    }
     w.load_file();
     w.show();
     return a.exec();
