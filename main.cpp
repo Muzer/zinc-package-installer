f88c27267b6f743aa993188015603b41b151ceea
muzerakascooby <muzerakascooby@29953a18-a857-11de-8ba5-91705c1537ed>
2009-09-24 15:57:08 +0000

    Added better argument framework. Multiple files (crudely) supported

--- main.cpp
+++ main.cpp
@@ -5,11 +5,25 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    int arg_loop;
     if (argc > 1)
     {
-        w.set_filename(string(argv[1]));
+        for(arg_loop = 1; arg_loop < argc; arg_loop++){
+            if(argv[arg_loop][0] != '-'){
+                w.set_filename(string(argv[arg_loop]));
+                w.load_file();
+                w.show();
+                a.exec();
+                while(!w.isHidden()){
+                    sleep(1);
+                }
+            } else {
+                // parsing code
+            }
+        }
+    } else {
+        w.load_file();
+        w.show();
+        return a.exec();
     }
-    w.load_file();
-    w.show();
-    return a.exec();
 }
