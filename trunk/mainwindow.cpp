429fb67f9dfa20899872a270f7047b4d50bdfc0c
TomMan81 <TomMan81@29953a18-a857-11de-8ba5-91705c1537ed>
2009-09-24 15:39:13 +0000

    Added support for displaying errors with non supported files.

--- trunk/mainwindow.cpp
+++ trunk/mainwindow.cpp
@@ -7,6 +7,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     filename = "none";
     app_version = "0.1 SVN";
+    accepted_formats[0] = ".deb";
 }
 
 MainWindow::~MainWindow()
@@ -35,12 +36,24 @@ void MainWindow::load_file()
     filename = file.toStdString();
     ui->lbl_app_name->setText(QString(filename.c_str()));
     ui->lbl_filename->setText(QString(filename.c_str()));
-    ui->lbl_filetype->setText(QString(get_file_type().c_str()));
+    string filetype = get_file_type();
+    if (array_find(accepted_formats, 1, filetype) == -1)
+    {
+        QErrorMessage msg;
+        msg.setWindowTitle("Format not supported!");
+        string message = "The format \"." + filetype + "\" isn't currently supported. If you think the format should be supported, submit a bug at code.google.com/p/zin-package-installer.";
+        msg.showMessage(QString(message.c_str()));
+        msg.exec();
+        ui->btn_install->setEnabled(false);
+    }
+    ui->lbl_filetype->setText(filetype.c_str());
 }
 
 string MainWindow::get_file_type()
 {
     string filetype = "???";
+    unsigned int end = split_string(filename, ".").size();
+    filetype = split_string(filename, ".")[end - 1];
     return filetype;
 }
 
@@ -54,4 +67,35 @@ void MainWindow::show_about_dialog()
     text.append(app_version.c_str());
     about_dialog.setDetailedText(text);
     about_dialog.exec();
+}
+
+// Functions
+
+int array_find(string array[], int length, string target)
+{
+    int flag = -1;
+    int i;
+    for (i = 0; i < length; i++)
+    {
+        if (target.compare(array[i].c_str()) == 0)
+        {
+            flag = i;
+        }
+    }
+    return flag;
+}
+
+vector<string> split_string(string str, string token)
+{
+    vector<string> strings;
+    int len = 0;
+    unsigned int pos = str.find(token, len);
+    while (pos != string::npos)
+    {
+        strings.push_back(str.substr(len, pos));
+        str.erase(len, pos + 1);
+        pos = str.find(token, len);
+    }
+    strings.push_back(str);
+    return strings;
 }
--- trunk/mainwindow.h
+++ trunk/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <iostream>
+#include <QtGui/QErrorMessage>
 #include <QtGui/QMainWindow>
 #include <QtGui/QMessageBox>
 #include <QtGui/QFileDialog>
@@ -28,9 +29,13 @@ private:
     Ui::MainWindow *ui;
     string filename;
     string app_version;
+    string accepted_formats[1];
 
 private slots:
     void show_about_dialog();
 };
+
+int array_find(string array[], int length, string target);
+vector<string> split_string(string str, string token);
 
 #endif // MAINWINDOW_H
