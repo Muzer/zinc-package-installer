47a3843f34a8f2e0a2b54fc48d0eec268b192e14
TomMan81 <TomMan81@29953a18-a857-11de-8ba5-91705c1537ed>
2009-09-25 16:46:48 +0000

    Added basic Pipe class, doesn't seem to work yet.

--- trunk/pipe.cpp
+++ trunk/pipe.cpp
@@ -3,4 +3,32 @@
 Pipe::Pipe(string cmd)
 {
     command = cmd;
+    pipe = popen(cmd.c_str(), "w");
+    if (!pipe)
+    {
+        cout << "Cannot open pipe." << endl;
+        exit(1);
+    }
+}
+
+string Pipe::read_string()
+{
+    char buffer[1000];
+    string result = "";
+    while (!feof(pipe))
+    {
+        if (fgets(buffer, sizeof(buffer), pipe) != NULL)
+                result += buffer;
+    }
+    return result;
+}
+
+bool Pipe::possible_to_read()
+{
+    return feof(pipe);
+}
+
+void Pipe::close()
+{
+    pclose(pipe);
 }
